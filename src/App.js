bf6efb098560f6e1566cf40ee3574f2143e41897
Donovan Le <dtle647@gmail.com>
2021-02-19 14:05:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import './App.css';
 
 const App = () => {
+    const [questions, setQuestions] = useState([]);
+    const [input, setInput] = useState('');
+    const [pagesize, setPagesize] = useState(5);
+    const [filter, setFilter] = useState('javascript');
+
+    const token = localStorage.getItem('token');
+
+    const handleSubmit = () => {
+        localStorage.setItem('token', 'hellomyfriend');
+    };
+
+    const getQuestions = async () => {
+        const res = await fetch(
+            `https://api.stackexchange.com/2.2/questions?page=1&pagesize=5&todate=1613692800&order=desc&min=1613692800&sort=activity&tagged=${filter}&site=stackoverflow`
+        );
+
+        const data = await res.json();
+
+        if (!data) {
+            console.log('no data available');
+        }
+
+        setQuestions(data.items);
+    };
+
+    const handleFilterChange = () => {};
+
+    useEffect(() => {
+        getQuestions();
+    }, [filter]);
+
     return (
         <div>
             <StyledWrapper>
-                <div>
-                    <h1>Stack Overflow</h1>
-                    <form>
-                        <input type='text' placeholder='Username...'></input>
-                        <input type='text' placeholder='Password...'></input>
-                        <button type='submit'>Submit</button>
-                    </form>
-                </div>
+                {!token ? (
+                    <div>
+                        <h1>Stack Overflow</h1>
+                        <form onSubmit={() => handleSubmit()}>
+                            <input
+                                type='text'
+                                placeholder='Username...'
+                            ></input>
+                            <input
+                                type='text'
+                                placeholder='Password...'
+                            ></input>
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </div>
+                ) : (
+                    <div className='questions'>
+                        <div>
+                            <div>
+                                <input
+                                    type='text'
+                                    placeholder='Change question filter'
+                                    value={input}
+                                    onChange={(e) => setInput(e.target.value)}
+                                ></input>
+                                <button
+                                    onClick={() => {
+                                        setFilter(input);
+                                        setInput('');
+                                    }}
+                                >
+                                    Filter
+                                </button>
+                            </div>
+                            <span>Filter by: {filter} Tag</span>
+                        </div>
+                        {questions ? (
+                            questions.map((question, index) => {
+                                return (
+                                    <div className='each-question' key={index}>
+                                        <span className='views'>
+                                            {question.views
+                                                ? question.views
+                                                : '0'}{' '}
+                                            Views
+                                        </span>
+                                        <span className='answers'>
+                                            {question.answer_count
+                                                ? question.answer_count
+                                                : '0'}{' '}
+                                            Answers
+                                        </span>
+                                        <span className='title'>
+                                            {question.title}
+                                        </span>
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <span>No questions with that tag</span>
+                        )}
+                    </div>
+                )}
             </StyledWrapper>
         </div>
     );
@@ -44,6 +130,40 @@ const StyledWrapper = styled.div`
 
         input {
             margin-bottom: 1rem;
+        }
+    }
+
+    .questions {
+        background-color: white;
+        padding: 0.5rem;
+        margin-top: 1rem;
+        border: solid 2px #f48023;
+        width: 80%;
+    }
+
+    .each-question {
+        display: flex;
+        align-items: center;
+        margin-bottom: 4rem;
+        margin-top: 3rem;
+        text-align: left;
+        border: solid 2px #bdc2c5;
+        padding: 0.5rem;
+        height: 4rem;
+        .views {
+            border: solid 1px black;
+            padding: 0.5rem;
+        }
+
+        .answers {
+            border: solid 1px black;
+            padding: 0.5rem;
+            margin-right: 0.5rem;
+        }
+
+        .title {
+            width: auto;
+            word-wrap: break-word;
         }
     }
 `;
